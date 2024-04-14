8f2a44478a395d736d44e5001357880f79a5ac1a
bobmagicii <bob@majdak.net>
2024-04-13 19:52:48 -0500

    thusfar

--- core/Local/Seekers/FindUniqueLinksHTML.php
+++ core/Local/Seekers/FindUniqueLinksHTML.php
@@ -2,12 +2,17 @@
 
 namespace Local\Seekers;
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 use Local;
 use Nether\Browser;
 use Nether\Common;
-use Nether\Console;
 
 use DOMElement;
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 class FindUniqueLinksHTML
 extends Local\Seeker {
@@ -16,19 +21,23 @@ extends Local\Seeker {
 	Run(mixed $Input):
 	mixed {
 
+		// until the attributes are done check that the step before this
+		// one generated a Browser Document object.
+
 		if(!($Input instanceof Browser\Document))
 		throw new Common\Error\FormatInvalid('Browser\Document');
 
-		$Found = [];
+		////////
+
 		$Links = $Input->Find('a[href]');
+		$Found = [];
 
 		$Links->Each(function(DOMElement $E) use(&$Found) {
-			$URL = $E->GetAttribute('href');
-			$Found[$URL] = TRUE;
+			$Found[] = $E->GetAttribute('href');
 			return;
 		});
 
-		$Found = array_keys($Found);
+		$Found = array_unique($Found);
 		sort($Found);
 
 		return $Found;
